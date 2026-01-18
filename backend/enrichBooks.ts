a4ecd8e139c672e87e365e2beb067b1c2cf3ee51
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2026-01-18 07:37:28 +0100

    wrote Google Books enrichment script

---
+++ backend/enrichBooks.ts
@@ -0,0 +1,69 @@
+import "dotenv/config";
+import { PrismaClient } from './generated/prisma/client/index.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
+import axios from 'axios';
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function enrich() {
+    // Search for the books with no description
+    const books = await prisma.book.findMany({
+        where: { description: null },
+        take: 100
+    });
+
+    console.log(`Starts enriching ${books.length}...`);
+
+    for (const book of books) {
+        try {
+            const queryTitle = book.title.split('('[0]?.trim());
+            const queryAuthor = book.author;
+
+            console.log(`Searching: ${queryTitle} - ${queryAuthor}`);
+
+            const response = await axios.get(`https://www.googleapis.com/books/v1/volumes`, {
+                params: {
+                    q: `intitle:${queryTitle}+inauthor:${queryAuthor}`,
+                    maxResults: 1
+                }
+            });
+
+            const data = response.data.items?.[0]?.volumeInfo;
+
+            if (data) {
+                await prisma.book.update({
+                    where: { id: book.id },
+                    data: {
+                        description: data.description || "No available description",
+                        publishingHouse: data.publisher || "Unknown",
+                        publishedDate: data.publisheddDate || "N.D.",
+                        coverUrl: data.imageLinks?.thumbnail?.replace('http:', 'https:') || null
+                    }
+                });
+                console.log(`✅ [Updated] ${book.title}`);
+            } else {
+                console.log(`⚠️ [No data available] ${book.title}`)
+                await prisma.book.update({
+                    where: { id: book.id },
+                    data: { description: "Description not found on Google Books"}
+                });
+            }
+
+            await delay(1000);
+        } catch (error) {
+            console.error(`Error during enrichment of ${book.title}:`, error);
+        }
+    }
+}
+
+enrich().finally(() => {
+    prisma.$disconnect();
+    pool.end();
+});
